8f57b526b0cedc6fcaa183bb82b34b3e5cbf76d4
AkshataDM <oxta28@gmail.com>
2014-04-07 22:36:27 +0530

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -14,7 +14,8 @@ end
 group :test do
   gem 'selenium-webdriver', '2.35.1'
   gem 'capybara', '2.1.0'
-end
+  gem 'factory_girl_rails', '4.2.1'
+ end
 
 gem 'sass-rails', '4.0.1'
 gem 'uglifier', '2.1.1'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -72,4 +72,30 @@ describe "with a password that's too short" do
       specify { expect(user_for_invalid_password).to be_false }
     end
   end
+
+describe "signup" do
+
+ before { visit signup_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
 end
