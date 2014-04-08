6596e874968822585632e78ba7076ff4251fd65e
AkshataDM <oxta28@gmail.com>
2014-04-08 22:55:35 +0530

    Update, edit and destroy users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+    before_action :correct_user,   only: [:edit, :update]
+    before_action :admin_user,     only: :destroy
   def new
 @user = User.new
 
@@ -7,20 +10,53 @@ class UsersController < ApplicationController
    @user = User.find(params[:id])
 end
 def create
-     @user = User.new(user_params)    # Not the final implementation!
-    if @user.save
-     sign_in @user
-    flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+     def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_back_or user
     else
+      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
+  
+  end
+def edit
+@user = User.find(params[:id])
+end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+ def index
+    @users = User.paginate(page: params[:page])
   end
 
+
+
 private
+   def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
+end
+
